a08f1bee8631242235e2e000a00ce6366785105a
Stephen Miracle <smiracle@cltdpd-macsp04.amcity.com>
2017-10-18 17:36:57 -0400

    a few refactors and revisions

--- main.go
+++ main.go
@@ -2,26 +2,28 @@ package kekcontact
 
 
 type Company struct {
-	Name string
-	Phone string
-	Address string
-	City string
-	Region string
-	PostalCode string
-	Id string
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+	Address string `json:"address"`
+	City string `json:"city"`
+	Region string `json:"region"`
+	PostalCode string `json:"postal_code"`
+	CountryCode string `json:"country_code"`
+	Id string `json:"id"`
 	kekCompanyId string
 }
 
 type Contact struct {
-	Name string
-	Email string
-	Phone string
-	Id string
-	Address string
-	City string
-	Region string
-	PostalCode string
-	CountryCode string
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+	Id string `json:"id"`
+	Address string `json:"address"`
+	City string `json:"city"`
+	Region string `json:"region"`
+	PostalCode string `json:"postal_code"`
+	CountryCode string `json:"country_code"`
 	kekContactId string
-	*Company
+	Company Company
 }
